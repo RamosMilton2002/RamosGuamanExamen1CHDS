3995baa92b1162c1a426464397ab014a0a4c9f37
Milton <milton.ramos954@ist17dejulio.edu.ec>
2024-12-09 09:58:00 -0500

    cracion de la accion con el boton pra el servicio

--- app/src/main/java/com/example/ramosguaman1shds/MainActivity.java
+++ app/src/main/java/com/example/ramosguaman1shds/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.ramosguaman1shds;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,7 +42,23 @@ public class MainActivity extends AppCompatActivity {
         Texto = findViewById(R.id.txtYo);
         btnTex = findViewById(R.id.btnYo);
 
-        btnTex.setOnClickListener(v -> obtenerServicioWeb("http://172.21.192.1:3001/mensaje"));
+        btnTex.setOnClickListener(v -> obtenerServicioWeb("http://10.10.35.220:3000/ramos"));
     }
-    
-}
+   private void obtenerServicioWeb (String url){
+       StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+               response -> {
+
+                   Texto.setText(response);
+               },error -> {
+           Toast.makeText(getApplicationContext(), "Error: " + error.toString(), Toast.LENGTH_SHORT).show();
+       }) {
+           @Override
+           protected Map<String, String> getParams() throws AuthFailureError {
+               return new HashMap<>();
+           }
+       };
+       RequestQueue requestQueue = Volley.newRequestQueue(this);
+       requestQueue.add(stringRequest);
+   }
+   }
+
