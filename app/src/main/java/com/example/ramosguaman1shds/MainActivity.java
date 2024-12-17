2860f183cc06baaaf250b68ea49f4dd6890313c9
Milton <milton.ramos954@ist17dejulio.edu.ec>
2024-12-17 14:54:40 -0500

    Arreglo de boton de Milton

--- app/src/main/java/com/example/ramosguaman1shds/MainActivity.java
+++ app/src/main/java/com/example/ramosguaman1shds/MainActivity.java
@@ -20,13 +20,17 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView Texto;
-     Button btnTex;
+     Button btnTex, btnmil, btnsm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +45,25 @@ public class MainActivity extends AppCompatActivity {
 
         Texto = findViewById(R.id.txtYo);
         btnTex = findViewById(R.id.btnYo);
+        btnmil = findViewById(R.id.btnMil);
+        btnsm = findViewById(R.id.btnSum);
 
-        btnTex.setOnClickListener(v -> obtenerServicioWeb("http://10.10.35.220:3000/ramos"));
+        btnTex.setOnClickListener(v -> obtenerServicioWeb("http://10.10.33.47:3001/nombre"));
+        btnmil.setOnClickListener(v -> obtenerServicioWeb2("http://10.10.33.47:3001/milton"));
     }
    private void obtenerServicioWeb (String url){
-       StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-               response -> {
-
-                   Texto.setText(response);
+       StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                     response -> {
+                         try {
+                             JSONArray j=new JSONArray(response.toString());
+                             for (int i = 0; i <= j.length()-1; i++) {
+                                 JSONObject res = j.getJSONObject(i);
+                                 Texto.setText(res+"");
+                             }
+                         } catch (JSONException e) {
+                             throw new RuntimeException(e);
+                         }
+                         Texto.setText(response);
                },error -> {
            Toast.makeText(getApplicationContext(), "Error: " + error.toString(), Toast.LENGTH_SHORT).show();
        }) {
@@ -60,5 +75,26 @@ public class MainActivity extends AppCompatActivity {
        RequestQueue requestQueue = Volley.newRequestQueue(this);
        requestQueue.add(stringRequest);
    }
+
+
+    private void obtenerServicioWeb2(String url) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                response -> {
+                    // Mostrar directamente la respuesta en el TextView
+                    Texto.setText(response);
+                },
+                error -> {
+                    Toast.makeText(getApplicationContext(), "Error: " + error.toString(), Toast.LENGTH_SHORT).show();
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return new HashMap<>();
+            }
+        };
+
+        // Agregar la solicitud a la cola
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
    }
 
